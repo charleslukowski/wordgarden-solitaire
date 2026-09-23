ab732fe0c5148ca890f87ac8304f22ce361ee40f
Charles Lukowski <lukowski.2@gmail.com>
2026-09-23 00:21:14 -0400

    Refresh offline cache for saved-round migration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wordgarden-v7';
+const CACHE='wordgarden-v8';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>event.waitUntil(
