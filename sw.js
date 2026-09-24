16a7d28f788e0a1b9749ce824d77fb328f0cdbad
Charles Lukowski <lukowski.2@gmail.com>
2026-09-23 22:24:08 -0400

    Refresh cache for easier first level

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wordgarden-v21';
+const CACHE='wordgarden-v22';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>event.waitUntil(
