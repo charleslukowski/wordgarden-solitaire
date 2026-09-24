cd49f17c589ca2d3ecd60c64414fd59d60015042
Charles Lukowski <lukowski.2@gmail.com>
2026-09-23 21:47:05 -0400

    Refresh cache for crown rewards and easier first deal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wordgarden-v19';
+const CACHE='wordgarden-v20';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>event.waitUntil(
