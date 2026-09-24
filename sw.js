af62d82daa677b2331039c42b8713d4b456367f1
Charles Lukowski <lukowski.2@gmail.com>
2026-09-23 20:35:35 -0400

    Put category crown cards in the tableau stacks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wordgarden-v12';
+const CACHE='wordgarden-v13';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>event.waitUntil(
