994863a79cf0fc588735e9c2f21fb1745bb04988
Charles Lukowski <lukowski.2@gmail.com>
2026-09-23 20:20:40 -0400

    Put categories on crowns and add FreeCell holders

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wordgarden-v11';
+const CACHE='wordgarden-v12';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>event.waitUntil(
