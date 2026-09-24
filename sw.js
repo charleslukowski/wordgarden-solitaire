8e5ae89e13fcc636cc4d86b79fd25c02f9ffa31e
Charles Lukowski <lukowski.2@gmail.com>
2026-09-23 21:22:16 -0400

    Refresh cache for larger free cells

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wordgarden-v15';
+const CACHE='wordgarden-v16';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>event.waitUntil(
